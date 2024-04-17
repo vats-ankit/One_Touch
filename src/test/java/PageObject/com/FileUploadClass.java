efee42b94d42432e9bffc7d6c771b40a0a6ddc7c
vats-ankit <akvats007@gmail.com>
2024-04-17 06:59:44 +0530

    push

---
+++ src/test/java/PageObject/com/FileUploadClass.java
@@ -0,0 +1,32 @@
+package PageObject.com;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FileUploadClass 
+{
+	WebDriver Currentdriver;
+	public FileUploadClass(WebDriver Remotedriver) 
+	{
+		Currentdriver=Remotedriver;
+		PageFactory.initElements( Remotedriver,this);
+	}
+	@FindBy(id="item-7") WebElement button;
+	@FindBy(id="uploadFile") WebElement uploadfile;
+	public void button() throws Exception
+	{
+		
+		button.click();
+	}
+	public void uploadfile(String s1) throws Exception
+	{
+		
+		uploadfile.sendKeys(s1);
+	
+		
+	}
+
+}
